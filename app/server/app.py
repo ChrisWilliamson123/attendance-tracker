cc41c48f9f2d94a788ed604e56667808749b706c
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-10-23 21:50:29 +0100

    Added basic db code to app.

--- app/server/app.py
+++ app/server/app.py
@@ -1,37 +1,30 @@
-import random, ssl, sys
+import os, random, ssl, sys
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 from werkzeug.serving import run_simple
 
-
+ssl_enabled = True
 try:
   ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
   ctx.load_cert_chain('server.crt', 'server.key')
 except (FileNotFoundError):
   # No ssl cert was found, continue without TLS.
-  pass
-
-def get_hello():
-    greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Hallo', 'Hej']
-    return random.choice(greeting_list)
+  ssl_enabled = False
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'attendance-tracker'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    conn = mysql.connect()
-    cur = conn.cursor()
-    print(cur)
+    cur = mysql.connection.cursor()
+    a = cur.execute('''CREATE TABLE Attendees (ID int)''')
+    print(a)
     return render_template('index.html')
-
-@app.route('/hello')
-def hello():
-    return get_hello()
 
 if __name__ == '__main__':
     try:
@@ -42,4 +35,7 @@ if __name__ == '__main__':
       port = sys.argv[2]
     except:
       port = 5000
-    run_simple(ip, port, app, ssl_context=ctx)
+    if ssl_enabled:
+        run_simple(ip, port, app, ssl_context=ctx)
+    else:
+        run_simple(ip, port, app)

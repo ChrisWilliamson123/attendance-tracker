047f9cbc85f1d3a160635aed9f9cf29f8985fee4
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-10-24 23:17:17 +0100

    create_table test complete.

--- app/server/db.py
+++ app/server/db.py
@@ -4,8 +4,16 @@ class DatabaseManager():
     def __init__(self, database_file):
         self.connection = sqlite3.connect(database_file)
 
+    def close_connection(self):
+        self.connection.close();
+
     def field_list_to_string(self, field_list):
         return ','.join(['"{}" "{}"'.format(x[0], x[1]) for x in field_list])
+
+    def get_table_info(self, table_name):
+        cursor = self.connection.cursor()
+        query = 'PRAGMA table_info("{}")'.format(table_name)
+        return cursor.execute(query).fetchall()
 
     def create_table(self, name, fields):
         cursor = self.connection.cursor()
--- app/server/test/db.test.py
+++ app/server/test/db.test.py
@@ -1,18 +1,22 @@
-import unittest
+import datetime, unittest
 from server.db import DatabaseManager
 
 class TestDatabaseMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.test_db = DatabaseManager(':memory:')
+
     def test_create_table(self):
-        test_db = DatabaseManager(':memory:')
-        cursor = test_db.connection.cursor()
-        test_db.create_table('test', [('ID', 'INTEGER'), ('name', 'TEXT')])
-        columns = cursor.execute('PRAGMA table_info("test");').fetchall()
+        self.test_db.create_table('test', [('ID', 'INTEGER'), ('name', 'TEXT')])
+        columns = self.test_db.get_table_info('test')
 
         self.assertEqual(columns[0][1], 'ID')
         self.assertEqual(columns[0][2], 'INTEGER')
         self.assertEqual(columns[1][1], 'name')
         self.assertEqual(columns[1][2], 'TEXT')
+
+    def tearDown(self):
+        self.test_db.close_connection()
 
 
 if __name__ == '__main__':

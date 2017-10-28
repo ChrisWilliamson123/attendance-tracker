9f10d1d4ae638aaa1b7745b4d9f7fc2193a3384f
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-10-28 12:21:44 +0100

    Added basic DB methods and tests.

--- app/server/test/db.test.py
+++ app/server/test/db.test.py
@@ -15,6 +15,19 @@ class TestDatabaseMethods(unittest.TestCase):
         self.assertEqual(columns[1][1], 'name')
         self.assertEqual(columns[1][2], 'TEXT')
 
+    def test_insert_into_select_from(self):
+        self.test_db.create_table('test', [('ID', 'INTEGER'), ('name', 'TEXT')])
+        self.test_db.insert_into('test', (1, 'Chris'))
+        results = self.test_db.select_from('test', ['ID', 'name'])
+        self.assertEqual(results, [(1, 'Chris')])
+
+    def test_multi_insert(self):
+        data = [(x, 'test') for x in range(1, 101)]
+        self.test_db.create_table('test', [('ID', 'INTEGER'), ('name', 'TEXT')])
+        self.test_db.insert_into('test', data)
+        results = self.test_db.select_from('test', ['ID', 'name'])
+        self.assertEqual(results, data)
+
     def tearDown(self):
         self.test_db.close_connection()
 

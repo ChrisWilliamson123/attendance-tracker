b016ffb9a2c58f159ad1c990ac537e4442fd14d4
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-10-29 20:33:14 +0000

    Fixed more indenting :sleeping:

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,9 +1,9 @@
 export const quaggaConfig = {
-    inputStream : {
-        name : "Live",
-        type : "LiveStream"
-    },
-    decoder : {
-        readers : ["code_128_reader"]
-    }
+  inputStream : {
+    name : "Live",
+    type : "LiveStream"
+  },
+  decoder : {
+    readers : ["code_128_reader"]
+  }
 }

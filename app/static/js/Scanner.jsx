927f30e3676efdaf762e261e034aa4e3d1407787
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-10-29 20:29:02 +0000

    Fixed indentation in Scanner.

--- app/static/js/Scanner.jsx
+++ app/static/js/Scanner.jsx
@@ -9,85 +9,85 @@ import Col from 'react-bootstrap/lib/Col';
 import { quaggaConfig } from './utils';
 
 class Scanner extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        Quagga.onDetected(result => this.handleDetectedBarcode(result));
+    Quagga.onDetected(result => this.handleDetectedBarcode(result));
 
-        Quagga.onProcessed(result => {
-          console.log('processed a frame');
-          const canvas = this.canvas,
-                canvasContext = canvas.getContext("2d");
+    Quagga.onProcessed(result => {
+      const canvas = this.canvas,
+      canvasContext = canvas.getContext("2d");
 
-          if (result) {
-            if (result.boxes) {
-              canvasContext.clearRect(0, 0, parseInt(canvas.getAttribute("width"), 10), parseInt(canvas.getAttribute("height"), 10));
-              result.boxes.filter(function (box) {
-                return box !== result.box;
-              }).forEach(function (box) {
-                Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, canvasContext, {color: "green", lineWidth: 2});
-              });
-            }
-
-            if (result.box) {
-              Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, canvasContext, {color: "#00F", lineWidth: 2});
-            }
-
-            if (result.codeResult && result.codeResult.code) {
-              Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, canvasContext, {color: 'red', lineWidth: 3});
-            }
-          }
-        });
-
-        this.state = {
-          scanning: false
+      if (result) {
+        if (result.boxes) {
+          canvasContext.clearRect(0, 0, parseInt(canvas.getAttribute("width"), 10), parseInt(canvas.getAttribute("height"), 10));
+          result.boxes.filter(box => {
+            return box !== result.box;
+          }).forEach(box => 
+            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, canvasContext, {color: "green", lineWidth: 2})
+          );
         }
-    }
 
-    componentDidUpdate(prevProps, prevState) {
-      if (this.state.scanning) {
-        Quagga.init(quaggaConfig, err => {
-            if (err) throw err;
-            Quagga.start();
-        });
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, canvasContext, {color: "#00F", lineWidth: 2});
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, canvasContext, {color: 'red', lineWidth: 3});
+        }
       }
-      else if (prevState.scanning === true) {
-        Quagga.stop();
-      }
-    }
+    });
 
-    handleDetectedBarcode = barcode => {
-      this.setState({scanning: false});
-      this.props.incrementAttendance();
+    this.state = {
+      scanning: false
     }
+  }
 
-    startScanning = () => {
-      if (!this.state.scanning) this.setState({scanning: true});
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.scanning) {
+      Quagga.init(quaggaConfig, err => {
+        if (err) throw err;
+        Quagga.start();
+      });
     }
+    else if (prevState.scanning === true) {
+      Quagga.stop();
+    }
+  }
 
-    render() {
-        return (
-            <Grid>
-                <Row>
-                    <Col xs={12}>
-                        <div className="text-center">
-                            <Button bsSize="large" onClick={this.startScanning}>Start Scanning</Button>
-                        </div>
-                    </Col>
-                </Row>
-                { this.state.scanning ?
-                    <Row>
-                        <Col xs={12}>
-                            <div id="interactive" className="viewport">
-                                <video autoPlay="true" preload="auto" src="" muted="true" playsInline="true"></video>
-                                <canvas className="drawingBuffer" width="0" height="0" ref={canvas => {this.canvas = canvas}}></canvas>
-                            </div>
-                        </Col>
-                    </Row>
-                : null }
-            </Grid>
-        );
-    }
+  handleDetectedBarcode = barcode => {
+    this.setState({scanning: false});
+    this.props.incrementAttendance();
+  }
+
+  startScanning = () => {
+    if (!this.state.scanning) this.setState({scanning: true});
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <div className="text-center">
+              <Button bsSize="large" onClick={this.startScanning}>Start Scanning</Button>
+            </div>
+          </Col>
+        </Row>
+        { this.state.scanning ?
+          <Row>
+            <Col xs={12}>
+              <div id="interactive" className="viewport">
+                <video autoPlay="true" preload="auto" src="" muted="true" playsInline="true"></video>
+                <canvas className="drawingBuffer" width="0" height="0" ref={canvas => {this.canvas = canvas}}></canvas>
+              </div>
+            </Col>
+          </Row>
+          : null 
+        }
+      </Grid>
+    );
+  }
 }
 
 export default Scanner;
